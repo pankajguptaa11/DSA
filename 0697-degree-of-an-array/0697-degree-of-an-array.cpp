81d6ff52ca87a79ea7dd0823548fd3476dd7346a
pankajguptaa11 <123762947+pankajguptaa11@users.noreply.github.com>
2023-09-22 22:02:22 +0530

    Time: 61 ms (16.42%), Space: 28.2 MB (15.35%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.cpp
+++ 0697-degree-of-an-array/0697-degree-of-an-array.cpp
@@ -1,45 +1,66 @@
-#include <tuple>
-
 class Solution {
-    struct Info {
-        int count;
-        int startIdx;
-        int endIdx;
-
-        void updateEnd(int end) {
-            endIdx = end;
-            ++count;
-        }
-        int range() const {
-            return endIdx - startIdx + 1;
-        }
-    };
 public:
-    int findShortestSubArray(vector<int>& nums) {
-        unordered_map<int, Info> cache; // num, <count, startIdx, endIdx>
+    int findShortestSubArray(vector<int>& nums){
+       
+       int n = nums.size();
+       map<int, int>freq;         //[number , freq]      
+       int maxFreq = INT_MIN;
 
-        for (int i = 0; i < nums.size(); ++i) {
-            if (cache.find(nums[i]) != cache.end()) {
-                cache[nums[i]].updateEnd(i);
-            }
-            else {
-                cache[nums[i]] = Info{1, i, i};
-            }
-        }
-        int count = 0;
-        int shorted = 0;
-        for (auto& [k, v] : cache) {
-            if (count < v.count) {
-                count = v.count;
+       for(int i=0 ; i<n ; i++)
+       {
+           freq[nums[i]]++;
+           maxFreq = max(maxFreq, freq[nums[i]]);
+       }
 
-                shorted = v.range();
-            }
-            else if (count == v.count) {
-                if (shorted > v.range()) {
-                    shorted = v.range();
-                }
-            }
-        }
-        return shorted;
+       set<int>st;   //contains elements with maximum freq
+
+       for(auto it : freq)
+       {
+           if(it.second == maxFreq)
+           {
+               st.insert(it.first);
+           }
+       }
+
+       map<int, int>startInd, endInd;    //[element , starting index]
+
+       for(int i=0 ; i<n ; i++)
+       {
+           startInd[nums[i]] = -1;
+           endInd[nums[i]] = -1;
+
+       }
+
+       for(int i=0 ; i<n; i++)
+       {
+           if(startInd[nums[i]] == -1)
+           {
+               //it's the first time the element has occured
+               startInd[nums[i]] = i;
+           }
+       }
+
+       for(int i=n-1 ; i>=0 ; i--)
+       {
+           if(endInd[nums[i]] == -1)
+           {
+               //last index
+               endInd[nums[i]] = i;
+           }
+       }
+
+       int len = INT_MAX;
+
+       for(int i=0 ; i<n ; i++)
+       {
+           if(st.find(nums[i]) != st.end())     //only maxFreq element 
+           {
+
+              len = min(len , endInd[nums[i]] - startInd[nums[i]] + 1);
+           }
+       }
+
+       return len;
+        
     }
 };
