cc75bbd55dab31954ea59c9ee397d9981188256e
pankajguptaa11 <123762947+pankajguptaa11@users.noreply.github.com>
2023-10-02 17:03:33 +0530

    Time: 12 ms (31.22%), Space: 6.2 MB (24.71%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-              string s = to_string(x);
-              for(int i=0,j=(s.size())-1;i<(s.size())/2;i++)
-              { 
-                if(s[i]!=s[j-i])
-                return false;
-                 
-                 
-              }
-              
-    return true;
+        long long rev=0;
+        long long ori=x;
+        while(x>0){
+            int r=x%10;
+            x=x/10;
+            rev=rev*10+r;
+        }
+        if(rev==ori){
+            return true;
+        }
+        return false;
     }
 };
