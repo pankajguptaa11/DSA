9de1cb1185b7e1b9ce221ea2b0ca195296ff119b
pankajguptaa11 <123762947+pankajguptaa11@users.noreply.github.com>
2023-10-08 23:11:57 +0530

    Time: 34 ms (11.77%), Space: 72.4 MB (10.93%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,35 +1,52 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy=new ListNode();
-        ListNode* temp=dummy;
-        int carry=0;
-        while(l1!=NULL || l2!=NULL || carry){
-            int sum=0;
-            if(l1!=NULL){
-                sum+=l1->val;
-                l1=l1->next;
+      ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *a = nullptr;
+        short int p = 0;
+        int temp = 0;
+        for (int i = 0; l1 != nullptr || l2 != nullptr; i++) {
+            if (l1 != nullptr) {   
+                ListNode *a2 = new ListNode;
+                a2 -> val = l1 -> val;
+                l1 = l1 -> next;
+                a2 -> next = a;
+                a = a2;
+            } else if (l2 != nullptr && l1 == nullptr) {
+                p =1;
+                ListNode *a2 = new ListNode;
+                a2 -> val = l2 -> val;
+                l2 = l2 -> next;
+                a2 -> next = a;
+                a = a2;
             }
-            if(l2!=NULL){
-                sum+=l2->val;
-                l2=l2->next;
+            if (l2 != nullptr && p == 0) {
+                a -> val += l2 -> val;
+                l2 = l2 -> next;
+            } else p = 0;
+            cout << a -> val << endl;
+            a -> val += temp;
+            temp = 0;
+            if (a -> val >= 10) {
+                temp = a -> val / 10;
+                a -> val = a -> val % 10;
             }
-            sum+=carry;
-            carry=sum/10;
-            ListNode* newnode=new ListNode(sum%10);
-            temp->next=newnode;
-            temp=temp->next;
         }
-        return dummy->next;
+        if (temp != 0) {
+            ListNode *a2 = new ListNode;
+            a2 -> val = temp;
+            a2 -> next = a;
+            a = a2;
+        }
+        ListNode *b = nullptr;
+        while (a != nullptr) {
+            ListNode *temp = a -> next;
+            ListNode *b2 = new ListNode;
+            b2 -> val = a -> val;
+            b2 -> next = b;
+            b = b2;
+            delete a;
+            a = temp;
+        }
+        return b;
     }
 };
