77d3f5675409896cea0b0e7f0a85690ca2c3537f
pankajguptaa11 <123762947+pankajguptaa11@users.noreply.github.com>
2023-10-10 20:01:34 +0530

    Time: 5 ms (18.23%), Space: 6.8 MB (8.14%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,22 +1,37 @@
+
 class Solution {
 public:
+    long Reverse(long x,int len)
+    {
+        if(len<=1)
+            return x;
+        
+        int a= 0; int b = 0;
+        int r = len/2;
+        int s = len - len/2;
+        int m=round(pow(10,r));
+        int M = round(pow(10,s));
+
+        a = x/m;
+        b = x%m;
+        x = (long)((long)(Reverse(b,r)*M) + (long)Reverse(a,s));
+        return x;
+    }
     int reverse(int x) {
-           // To check for overflow
-         // To check for overflow
-        
-        int sum = 0;
-        while (x != 0) {
-            int rem = x % 10;
-            
-            // Check for overflow before updating sum
-            if((sum>INT_MAX / 10) ||(sum<INT_MIN / 10)){
-                return 0;
-            }
-            
-            sum = sum * 10 + rem;
-            x = x / 10;
+        ios_base::sync_with_stdio(0);
+        int len=0;
+        long y;
+
+            y =x;
+        while(y!=0)
+        {
+            y = y/10;
+            len++;
         }
-        
-        return sum;
+        y = x;
+        y = Reverse(y,len);
+        if(y>pow(2,31)-1||y<-pow(2,31)) return 0;
+
+        return y;
     }
 };
