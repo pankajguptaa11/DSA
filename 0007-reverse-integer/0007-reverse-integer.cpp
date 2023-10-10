6c99de3414a1977d3680273c794f4c2f33b9de10
pankajguptaa11 <123762947+pankajguptaa11@users.noreply.github.com>
2023-10-10 20:00:20 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (15.99%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int reverse(int x) {
+           // To check for overflow
+         // To check for overflow
+        
+        int sum = 0;
+        while (x != 0) {
+            int rem = x % 10;
+            
+            // Check for overflow before updating sum
+            if((sum>INT_MAX / 10) ||(sum<INT_MIN / 10)){
+                return 0;
+            }
+            
+            sum = sum * 10 + rem;
+            x = x / 10;
+        }
+        
+        return sum;
+    }
+};
