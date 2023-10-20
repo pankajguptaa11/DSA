ce1a0ddf0914c868d370715c6241ba599a1f04ee
pankajguptaa11 <123762947+pankajguptaa11@users.noreply.github.com>
2023-10-20 21:57:28 +0530

    Time: 1445 ms (97.38%), Space: 0B (100.00%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,9 +1,10 @@
-SELECT s.student_id, s.student_name, sub.subject_name, COALESCE(e.attended_exams, 0) AS attended_exams
+SELECT 
+    s.student_id, 
+    s.student_name, 
+    u.subject_name, 
+    COUNT(e.subject_name) AS attended_exams
 FROM Students s
-CROSS JOIN Subjects sub
-LEFT JOIN (
-    SELECT student_id, subject_name, COUNT(*) AS attended_exams
-    FROM Examinations
-    GROUP BY student_id, subject_name
-) e ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
-ORDER BY s.student_id, sub.subject_name;
+JOIN Subjects u
+LEFT JOIN Examinations e ON s.student_id = e.student_id AND u.subject_name = e.subject_name
+GROUP BY s.student_id, u.subject_name
+ORDER BY s.student_id, u.subject_name;
