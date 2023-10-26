f8b7f1247b990da367c286cb8cbc758848606f09
Pankaj Gupta <123762947+pankajguptaa11@users.noreply.github.com>
2023-10-26 18:10:14 +0530

    Time: 5 ms (20.20%), Space: 7.1 MB (32.10%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
@@ -1,58 +1,25 @@
 class Solution {
 public:
     vector<int> shortestToChar(string s, char c) {
-        vector<int>left(s.size(),-1);
-        int j=0;
-        int flag=0;
-        for(int i=0;i<s.size();i++)
+        vector<int> position;
+        vector<int> answer;
+        
+        for(int i=0; i<s.size(); i++)
         {
-            if(s[i]==c && flag==1)
-            {
-                left[i]=0;
-                j=0;
-            }
             if(s[i]==c)
-            {
-                left[i]=0;
-                j=1;
-                flag=1;
-            }
-            else if(s[i]!=c && flag==1)
-            {
-                left[i]=j++;
-            }
+                position.push_back(i);
         }
-        vector<int>right(s.size(),-1);
-        j=0;
-        flag=0;
-        for(int i=s.size()-1;i>=0;i--)
+        
+        for(int i=0; i<s.size(); i++)
         {
-            if(s[i]==c && flag==1)
+            int shortest_dist = INT_MAX;
+            for(int j=0; j<position.size(); j++)
             {
-                right[i]=0;
-                j=0;
+                shortest_dist = min(shortest_dist, abs(i-position[j]));
             }
-            if(s[i]==c)
-            {
-                right[i]=0;
-                j=1;
-                flag=1;
-            }
-            else if(s[i]!=c && flag==1)
-            {
-                right[i]=j++;
-            }
+            answer.push_back(shortest_dist);
         }
-        vector<int>hny;
-        for(int i=0;i<s.size();i++)
-        {
-            if(left[i]>=0 && right[i]>=0)
-            {
-                hny.push_back(min(left[i],right[i]));
-            }
-            else 
-              hny.push_back(max(left[i],right[i]));
-        }
-        return hny;
+        
+        return answer;
     }
 };
