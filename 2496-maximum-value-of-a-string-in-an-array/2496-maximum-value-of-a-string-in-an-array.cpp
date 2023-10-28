97c61141fe8fa45eb4b4a2fdc7b607002db97c32
Pankaj Gupta <123762947+pankajguptaa11@users.noreply.github.com>
2023-10-28 22:50:07 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (48.69%) - LeetHub

--- 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.cpp
+++ 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.cpp
@@ -1,39 +1,22 @@
 class Solution {
 public:
-    int findPattern( string s ){
-
-        bool hasDigit = false ;
-        bool hasChar = false ;
-        int maxVal = 0;
-
-        for( int i=0 ; i<s.size() ; i++ ){
-
-            if( s[i]-'0' >= 49 ) hasChar = true ;
-            else if( s[i]-'0'>=0 && s[i]-'0' <= 9 ){
-                hasDigit = true ;
-                maxVal = maxVal*10 + s[i]-'0';
-            }
-        }
-
-        if( hasDigit && hasChar ) return s.size() ;
-        else if( !hasDigit && hasChar ) return s.size() ;
-
-        return maxVal ;
-    }
-
     int maximumValue(vector<string>& strs) {
-        int ans = 0 ;
-        vector<int> v(strs.size() );
-        
-        for( int i=0 ; i<strs.size() ; i++ ){
-            
-            v[i] = findPattern( strs[i] );
-        }
-
-        for( auto it : v ){
-            ans = max( ans, it );
-        }
-
-        return ans ;
+        int max = 0;
+	    for (int i = 0; i < strs.size(); i++) {
+	    	string text = "1" + strs[i]; // add a 1 in the beginning to handle cases that starts with 0's
+	    	double value1 = stod(text + "0");
+	    	double value2 = stod(text) * 10;
+	    	if (value1 == value2) {
+	    		// integer
+	    		int value = stoi(strs[i]);
+	    		max = (max < value ? value : max);
+	    	}
+	    	else {
+	    		// string
+	    		int length = strs[i].length();
+	    		max = (max < length ? length : max);
+	    	}
+	    }
+	    return max;
     }
 };
