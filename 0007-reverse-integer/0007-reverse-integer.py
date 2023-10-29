231e4befca7e800a636b372ad46d8b3b6fe903bc
Pankaj Gupta <123762947+pankajguptaa11@users.noreply.github.com>
2023-10-29 21:39:13 +0530

    Time: 31 ms (95.39%), Space: 16.3 MB (40.02%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,23 +1,8 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        isNeg = False
-        arr = []
-        b = ""
-        if len(str(x)) == 1:
-            return x
-        while x % 10 == 0:
-            x = int(x / 10)
-        if x < 0:
-            isNeg = True
-            x *= -1
-        for i in str(x):
-            arr.append(i)
-        for j in arr[::-1]:
-            b += j
-        c = int(b)
-        if isNeg:
-            c *= -1
-        if c.bit_length() > 31:
-            return 0
-        else:
-            return c
+        flag = -1 if x < 0 else 1
+
+        s = str(abs(x))
+        x = int(s[::-1])
+
+        return x*flag if x < 2**31 else 0
