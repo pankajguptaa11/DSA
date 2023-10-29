24fe5e663bdbf9f62bd0799dd7d0a87a4b232d0c
Pankaj Gupta <123762947+pankajguptaa11@users.noreply.github.com>
2023-10-29 21:38:29 +0530

    Time: 43 ms (37.47%), Space: 16.3 MB (40.02%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,14 +1,23 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        MAX_INT = 2 ** 31 - 1 # 2,147,483,647
-        MIN_INT = -2 ** 31    #-2,147,483,648
-        reverse = 0
-
-        while x != 0:
-            if reverse > MAX_INT / 10 or reverse < MIN_INT / 10:
-                return 0
-            digit = x % 10 if x > 0 else x % -10
-            reverse = reverse * 10 + digit
-            x = math.trunc(x / 10)
-
-        return reverse
+        isNeg = False
+        arr = []
+        b = ""
+        if len(str(x)) == 1:
+            return x
+        while x % 10 == 0:
+            x = int(x / 10)
+        if x < 0:
+            isNeg = True
+            x *= -1
+        for i in str(x):
+            arr.append(i)
+        for j in arr[::-1]:
+            b += j
+        c = int(b)
+        if isNeg:
+            c *= -1
+        if c.bit_length() > 31:
+            return 0
+        else:
+            return c
